ffd92abff796637da8d9cb7e978809ebd30b0bea
pial-nf <personalemail2106263@gmail.com>
2025-01-26 17:29:25 +0600

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : " mongodb+srv://shivashiva@musicbot.ty0ul.mongodb.net/?retryWrites=true&w=majority",
+  mongodbUri : "mongodb+srv://shivashiva@musicbot.ty0ul.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
